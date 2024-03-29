f810d560c6b0fd70fd84d2e90d4dc092fba2a8cf
Robert McLaws <1657085+robertmclaws@users.noreply.github.com>
2024-03-29 18:44:30 -0400

    Northwind testing has been done through Microsoft-hosted services for a while now,so this needs to go away.

--- src/Simple.OData.Tests.Shared.ProductService/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Simple.OData.Tests.Shared.ProductService.WebApiApplication" Language="C#" %>
--- src/Simple.OData.Tests.Shared.ProductService/Models/BaseModel.cs
+++
@@ -1,7 +0,0 @@
-﻿// ReSharper disable CheckNamespace
-namespace Simple.OData.Tests.Shared.ProductService.Models;
-
-public abstract class BaseModel
-{
-	public Guid Id { get; set; }
-}
